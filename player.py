5450099951d9827460f48eb994f49d1d1bc50251
r0663r <duvnjakigor6@gmail.com>
2025-12-16 13:20:25 +0100

    Adding forward and backward moving

--- player.py
+++ player.py
@@ -1,6 +1,11 @@
 import pygame
 
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    LINE_WIDTH,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED
+)
 from circleshape import CircleShape
 
 
@@ -21,6 +26,12 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
     
     def draw(self, screen):
         points_list = self.triangle()
@@ -34,4 +45,10 @@ class Player(CircleShape):
         
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
